9fbccdb9eb3d72fa38057aa953b1aac170540d41
cyvac1 <cyvac1@jinhakapply.com>
2025-07-31 10:19:08 +0900

    ✨ 장치 테스트 페이지 추가

--- src/pages.ts
+++ src/pages.ts
@@ -1,4 +1,5 @@
 import MimeTypePage from "./pages/MimeTypePage";
+import DeviceTestPage from "./pages/DeviceTestPage";
 
 const pages: {
   name: string;
@@ -11,6 +12,12 @@ const pages: {
     path: "/mime-type",
     description: "브라우저의 MIME 타입 지원 여부를 확인할 수 있습니다.",
     component: MimeTypePage,
+  },
+  {
+    name: "장치 테스트 페이지",
+    path: "/device-test",
+    description: "카메라, 마이크, 스피커 테스트를 할 수 있습니다.",
+    component: DeviceTestPage,
   },
 ];
 
---
+++ src/pages/DeviceTestPage.tsx
@@ -0,0 +1,3 @@
+export default function DeviceTestPage() {
+  return <div>DeviceTestPage</div>;
+}
