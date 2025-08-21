ff752fc3717ed1baf3ae67e64d10ee9a5b29b45c
cyvac1 <cyvac1@jinhakapply.com>
2025-08-21 10:49:29 +0900

    ⚡️ useNoiseSuppression 로직 개선

--- src/hooks/useNoiseSuppression.ts
+++ src/hooks/useNoiseSuppression.ts
@@ -18,42 +18,58 @@ export default function useNoiseSuppression({ enabled, audioStream }: UseNoiseSu
   const [stream, setStream] = useState<MediaStream | null>(null);
 
   useEffect(() => {
-    if (!enabled || !audioStream) {
-      return;
-    }
+    let isMounted = true;
 
     let audioContext: AudioContext;
     let sourceNode: MediaStreamAudioSourceNode;
     let destinationNode: MediaStreamAudioDestinationNode;
     let rnnoiseNode: AudioWorkletNode;
 
-    const init = async () => {
+    const getStream = async () => {
+      if (!audioStream) return;
       audioContext = new AudioContext({ sampleRate: 48000 });
       sourceNode = audioContext.createMediaStreamSource(audioStream);
       destinationNode = audioContext.createMediaStreamDestination();
 
       await window.RNNoiseNode.register(audioContext);
-      rnnoiseNode = new window.RNNoiseNode(audioContext);
-      sourceNode.connect(rnnoiseNode);
-      rnnoiseNode.connect(destinationNode);
+      if (isMounted) {
+        rnnoiseNode = new window.RNNoiseNode(audioContext);
+        sourceNode.connect(rnnoiseNode);
+        rnnoiseNode.connect(destinationNode);
 
-      setStream(destinationNode.stream);
-    };
-
-    init();
-
-    return () => {
-      if (rnnoiseNode) {
+        setStream(destinationNode.stream);
+      } else {
+        // 언마운트 시 즉시 노드 해제
         rnnoiseNode.disconnect();
-      }
-      if (sourceNode) {
+        destinationNode.disconnect();
         sourceNode.disconnect();
-      }
-      if (audioContext) {
         audioContext.close();
       }
     };
+
+    // 활성화 상태라면 스트림 가져오기
+    if (enabled) {
+      getStream();
+    }
+    // 비활성화 상태라면 스트림 초기화
+    else {
+      setStream(null);
+    }
+
+    return () => {
+      isMounted = false;
+      rnnoiseNode?.disconnect();
+      destinationNode?.disconnect();
+      sourceNode?.disconnect();
+      audioContext?.close();
+    };
   }, [enabled, audioStream]);
+
+  useEffect(() => {
+    return () => {
+      stream?.getAudioTracks().forEach((track) => track.stop());
+    };
+  }, [stream]);
 
   return { stream: enabled ? stream : audioStream };
 }
